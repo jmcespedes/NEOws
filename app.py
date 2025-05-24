aba10432a260ffbf7a0c4a747cd94f9de7121b11
jmcespedes <cespedes.josem@gmail.com>
2025-05-24 19:44:45 -0400

    actualiza

--- app.py
+++ app.py
@@ -76,8 +76,11 @@ def enviar_mensajes_pendientes():
             for nombre_prov, telefono, nombre_servicio in proveedores:
                 mensaje = (
                     f"👋 Hola {nombre_prov}, soy de NEOServicios. Tienes una nueva solicitud del servicio *{nombre_servicio}* en *{comuna_nombre}*:\n\n"
+                    f"📩 *Solicitud del cliente:* {pregunta_cliente}\n"
+                    f"📞 *Contacto:* {celular}\n\n"
                     f"¿Deseas tomar el servicio? Responde con *SÍ* o *NO*."
                 )
+
                 try:
                     client.messages.create(
                         body=mensaje,
